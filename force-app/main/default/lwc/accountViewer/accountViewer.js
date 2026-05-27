bdeb1fb4de53ce57b149e94874a6bf3a46aeb209
ankit patel <akpatel3611@gmail.com>
2026-05-27 13:29:23 +0530

    test: validate dependency precommit flow
    test: validate dependency precommit flow

--- force-app/main/default/lwc/accountViewer/accountViewer.js
+++ force-app/main/default/lwc/accountViewer/accountViewer.js
@@ -1,4 +1,10 @@
 import { LightningElement } from 'lwc';
 import getRecentOperations from '@salesforce/apex/AsyncOperationController.getRecentOperations';
+
 export default class AccountViewer extends LightningElement {
+
+    connectedCallback() {
+        console.log('dependency test');
+    }
+
 }
